15d770b6e45d3503a4c5af431099c666819f4bad
yindaheng98 <yindaheng98@gmail.com>
2022-07-17 15:17:01 +0800

    use MultiGraph

--- dblp_crawler/graph.py
+++ dblp_crawler/graph.py
@@ -1,40 +1,14 @@
 import asyncio
 import abc
+from itertools import combinations
 from dblp_crawler import download_person, DBLPPerson
 import networkx as nx
-
-
-class EdgeDict:
-    def __init__(self):
-        self.edges = {}
-
-    @staticmethod
-    def key(a: str, b: str):
-        return max(a, b) + "," + min(a, b)
-
-    @staticmethod
-    def value(a: str, b: str):
-        return max(a, b), min(a, b)
-
-    def add(self, a: str, b: str, publication):
-        k = self.key(a, b)
-        if k not in self.edges:
-            self.edges[k] = {
-                "edge": self.value(a, b),
-                "publication": []
-            }
-        self.edges[k]["publication"].append(publication)
-
-    def draw(self, g: nx.Graph):
-        for k, v in self.edges.items():
-            g.add_edge(*v["edge"], weight=len(v["publication"]))
 
 
 class Graph(metaclass=abc.ABCMeta):
     def __init__(self, pid: str):
         self.persons = {pid: None}
         self.checked = set()
-        self.edges = EdgeDict()
         self.publications = {}
 
     @abc.abstractmethod
@@ -50,34 +24,37 @@ class Graph(metaclass=abc.ABCMeta):
         for pid, person in list(self.persons.items()):
             if person is None:
                 tasks.append(asyncio.create_task(self.download_person(pid)))
-                continue  # 还没写入的节点先写入
+                continue  # 还没下载的节点先下载
             if pid in self.checked:
-                continue  # 已经遍历过的不再重复
+                continue  # 已经遍历过的节点不再重复
             self.checked.add(pid)  # 记录下这个节点已遍历
             for publication in self.filter_publications(person.publications()):
                 if publication.key() in self.publications:
-                    continue  # 已经遍历过的不再重复
-                self.publications[publication.key()] = publication
+                    continue  # 已经遍历过的文章不再重复
+                self.publications[publication.key()] = publication  # 记录下这个文章已遍历
                 for author in publication.authors():
-                    if not author.pid() == pid:  # 如果不是自己
-                        self.edges.add(author.pid(), pid, publication)  # 就记录合作情况
                     if author.pid() not in self.persons:  # 如果作者不存在
                         tasks.append(asyncio.create_task(self.download_person(author.pid())))  # 就获取作者
                         self.persons[author.pid()] = None  # 并记录之
         await asyncio.gather(*tasks)
 
     def networkx(self):
-        g = nx.Graph()
+        g = nx.MultiGraph()
         for pid, person in self.persons.items():
-            g.add_node(pid, name=person.name())
-        self.edges.draw(g)
+            g.add_node(pid, data=person)
+        for _, publication in self.publications.items():
+            authors_pid = {author.pid() for author in publication.authors()}
+            for a, b in combinations(authors_pid, 2):
+                if a == b:
+                    continue
+                g.add_edge(a, b, key=publication.key(), publication=publication)
         return g
 
 
 if __name__ == "__main__":
     import logging
     import random
-    import matplotlib.pyplot as plt
+
     logging.basicConfig(level=logging.DEBUG)
 
 
@@ -97,15 +74,8 @@ if __name__ == "__main__":
         print("-" * 100)
         await g.bfs_once()
         print("-" * 100)
-        plt.figure(figsize=(8, 8))
-        networkx = g.networkx()
-
-        pos = nx.kamada_kawai_layout(networkx)
-        nx.draw_networkx_edges(networkx, pos, alpha=0.3, edge_color="m")
-        nx.draw_networkx_nodes(networkx, pos, node_color="#210070", alpha=0.9)
-        label_options = {"ec": "k", "fc": "white", "alpha": 0.7}
-        nx.draw_networkx_labels(networkx, pos, font_size=14, bbox=label_options)
-        plt.show()
+        x = g.networkx()
+        print(x)
 
 
     loop = asyncio.get_event_loop()
