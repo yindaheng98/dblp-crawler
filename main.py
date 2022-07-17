b3dcafda2efb4b82d99e356fe88ad70ac9189d39
yindaheng98 <yindaheng98@gmail.com>
2022-07-17 15:49:27 +0800

    draw it

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ keywords = [
 
 class GG(Graph):
     def filter_publications(self, publications):
-        return filter_publications_by_keywords(publications, keywords)
+        publications = filter_publications_by_keywords(publications, keywords)
+        publications = filter_publications_after(publications, 2020)
+        return publications
 
 
 async def main():
     g = GG('74/1552-1')
     for i in range(3):
         await g.bfs_once()
+    draw(g.networkx())
 
 
 if __name__ == "__main__":
