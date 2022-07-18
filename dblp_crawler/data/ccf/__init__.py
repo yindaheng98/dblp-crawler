41b84727143a3444879c6726b53c6b459d9dcef4
yindaheng98 <yindaheng98@gmail.com>
2022-07-18 22:37:56 +0800

    outputed

---
+++ dblp_crawler/data/ccf/__init__.py
@@ -0,0 +1,18 @@
+from .list import CCF_A as a
+from .list import CCF_B as b
+from .list import CCF_C as c
+from .matched import data as matched
+
+CCF_A = set()
+CCF_B = set()
+CCF_C = set()
+for k, v in matched.items():
+    r = ""
+    if v in a:
+        CCF_A.add(k)
+    elif v in b:
+        CCF_B.add(k)
+    elif v in c:
+        CCF_C.add(k)
+    else:
+        print(k, v)
