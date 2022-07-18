89d7334849a3150a097994931eea1eef2d365012
yindaheng98 <yindaheng98@gmail.com>
2022-07-19 01:01:09 +0800

    fixed them

---
+++ dblp_crawler/data/ccf/download.py
@@ -0,0 +1,23 @@
+import re
+from dblp_crawler.data.ccf.raw import *
+
+
+def key(r):
+    urls = re.findall(r"https*://.*$", r)
+    if len(urls) != 1:
+        print(urls, r)
+        return None
+    url = urls[0].replace(" ", "")
+    search = re.search(r"https*://dblp\.uni-trier.de/(.+?)$", url)
+    if search is None:
+        print(urls, r)
+        return None
+    return "/".join(search.group(1).split("/")[0:3])
+
+
+CCF_A = [key(r) for r in CCF_A]
+CCF_B = [key(r) for r in CCF_B]
+CCF_C = [key(r) for r in CCF_C]
+print(CCF_A)
+print(CCF_B)
+print(CCF_C)
