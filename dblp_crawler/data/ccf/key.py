e2c907f78c0ec6e70046422bca5a4d32d48fbf4f
yindaheng98 <yindaheng98@gmail.com>
2022-07-19 10:02:01 +0800

    CCF print

--- dblp_crawler/data/ccf/key.py
+++ dblp_crawler/data/ccf/key.py
@@ -1,22 +1,27 @@
 import re
 from .raw import *
 
+CCF = ''
+
 
 def key(r):
     urls = re.findall(r"https*://.*$", r)
     if len(urls) != 1:
-        print(urls, r)
+        print(CCF, urls, r)
         return None
     url = urls[0].replace(" ", "")
     search = re.search(r"https*://dblp\.uni-trier.de/(.+?)$", url)
     if search is None:
-        print(urls, r)
+        print(CCF, urls, r)
         return None
     return "/".join(search.group(1).split("/")[0:3])
 
 
+CCF = 'A'
 CCF_A = [key(r) for r in CCF_A]
+CCF = 'B'
 CCF_B = [key(r) for r in CCF_B]
+CCF = 'C'
 CCF_C = [key(r) for r in CCF_C]
 print(CCF_A)
 print(CCF_B)
