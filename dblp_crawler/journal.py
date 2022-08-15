e0d7f51ae39eaf75cd552459e5a22bf88245b3b8
yindaheng98 <yindaheng98@gmail.com>
2022-08-15 21:01:03 +0800

    parse all journal

--- dblp_crawler/journal.py
+++ dblp_crawler/journal.py
@@ -1,10 +1,11 @@
 import logging
 import xml.etree.ElementTree as ElementTree
+from dblp_crawler import Publication
 
 logger = logging.getLogger("parser")
 
 
-class DBLPJournal:
+class JournalList:
     def __init__(self, data: ElementTree.Element):
         assert data.tag == "bht", "Should be xml of a bht!"
         self.data = data
@@ -12,8 +13,21 @@ class DBLPJournal:
     def title(self):
         return self.data.attrib['title']
 
-    def journals(self):
+    def journal_keys(self):
         return [li.attrib["href"] for li in self.data.findall('./ul/li/ref')]
+
+    async def journals(self):
+        for jid in self.journal_keys():
+            yield Journal(await download_journal(jid))
+
+
+class Journal:
+    def __init__(self, data: ElementTree.Element):
+        assert data.tag == "bht", "Should be xml of a bht!"
+        self.data = data
+
+    def publications(self):
+        return [Publication(r) for r in self.data.findall('./dblpcites/r')]
 
 
 if __name__ == "__main__":
@@ -22,9 +36,15 @@ if __name__ == "__main__":
 
 
     async def main():
-        jl = DBLPJournal(await download_journal_list('db/journals/tmm'))
-        for journal in jl.journals()[0:3]:
-            print(await download_journal(journal))
+        jl = JournalList(await download_journal_list('db/journals/tmm'))
+        i = 0
+        async for journal in jl.journals():
+            i += 1
+            if i >= 3:
+                break
+            print(journal)
+            for publication in journal.publications():
+                print(publication)
 
 
     asyncio.run(main())
