d5c74a9017a02295e371d51550a5f425b37028a7
yindaheng98 <yindaheng98@gmail.com>
2022-08-15 20:13:24 +0800

    using pid

--- dblp_crawler/downloader.py
+++ dblp_crawler/downloader.py
@@ -5,6 +5,7 @@ import aiohttp
 import logging
 from aiofile import async_open
 from asyncio import Semaphore
+from .journal import DBLPJournal
 
 logger = logging.getLogger("downloader")
 
@@ -13,7 +14,15 @@ file_sem = Semaphore(64)
 
 
 async def download_person(pid: str):
-    save_path = os.path.join("save", pid + ".xml")
+    return await download_item("pid/" + pid + ".xml")
+
+
+async def download_journal_list(pid: str):
+    return await download_item(pid + "/index.xml")
+
+
+async def download_item(path: str):
+    save_path = os.path.join("save", path)
     if os.path.isfile(save_path):
         async with file_sem:
             try:
@@ -25,12 +34,12 @@ async def download_person(pid: str):
             except:
                 logger.debug(" no cache: %s" % save_path)
 
-    url = "https://dblp.org/pid/%s.xml" % pid
+    url = "https://dblp.org/" + path
     async with http_sem:
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as response:
-                    logger.info(" download: %s" % pid)
+                    logger.info(" download: %s" % path)
                     html = await response.text()
                     data = ElementTree.fromstring(html)
                     os.makedirs(os.path.dirname(save_path), exist_ok=True)
@@ -39,46 +48,3 @@ async def download_person(pid: str):
                     return data
         except Exception as e:
             logger.error("invalid response: %s" % e)
-
-
-cache = {}
-
-
-async def get_journal_full(pid: str):
-    if pid in cache:
-        return cache[pid]
-    data = await get_journal(pid)
-    cache[pid] = data.attrib['title']
-    return cache[pid]
-
-
-async def get_journal(pid: str):
-    if pid in cache:
-        return cache[pid]
-    pid = os.path.dirname(pid) + "/index.xml"
-    save_path = os.path.join("save", pid)
-    if os.path.isfile(save_path):
-        async with file_sem:
-            try:
-                async with async_open(save_path, 'r') as f:
-                    logger.debug("use cache: %s" % save_path)
-                    html = await f.read()
-                    data = ElementTree.fromstring(html)
-                    return data
-            except:
-                logger.debug(" no cache: %s" % save_path)
-
-    url = "https://dblp.org/%s" % pid
-    async with http_sem:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                logger.info(" download: %s" % pid)
-                html = await response.text()
-                if response.status != 200:
-                    logger.error("invalid response %s: %s" % (response.status, url))
-                    return None
-                data = ElementTree.fromstring(html)
-                os.makedirs(os.path.dirname(save_path), exist_ok=True)
-                async with async_open(save_path, 'w') as f:
-                    await f.write(html)
-                return data
