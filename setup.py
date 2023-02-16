54f54a36064a2235eec089b6ebe162483659c95a
yindaheng98 <yindaheng98@gmail.com>
2023-02-16 21:44:00 +0800

    publish

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+package_dir = {
+    'dblp_crawler': 'dblp_crawler',
+    'dblp_crawler.data': 'dblp_crawler/data',
+    'dblp_crawler.filter': 'dblp_crawler/filter',
+    'dblp_crawler.keyword': 'dblp_crawler/keyword',
+    'dblp_crawler.summarizer': 'dblp_crawler/summarizer',
+}
+
+setup(
+    name='dblp_crawler',
+    version='0.0.1',
+    author='yindaheng98',
+    author_email='yindaheng98@163.com',
+    url='https://github.com/yindaheng98/dblp-crawler',
+    description=u'异步高并发dblp爬虫，慎用',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir=package_dir,
+    packages=[key for key in package_dir],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'networkx==2.8.6',
+        'aiofile==3.8.1',
+        'aiohttp==3.8.1',
+        'matplotlib==3.5.3',
+        'neo4j==5.5.0',
+    ],
+)
