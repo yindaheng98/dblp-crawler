e9d6fff83033ef82918c317b1bc5c0d791bb4017
yindaheng98 <yindaheng98@gmail.com>
2023-02-17 17:18:28 +0800

    delete deprecated

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ package_dir = {
 
 setup(
     name='dblp_crawler',
-    version='0.0.1',
+    version='0.0.2',
     author='yindaheng98',
     author_email='yindaheng98@163.com',
     url='https://github.com/yindaheng98/dblp-crawler',
