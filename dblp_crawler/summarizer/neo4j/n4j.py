c356c39511fd28cd90ec87c8c144eb3eeee5ee22
yindaheng98 <yindaheng98@gmail.com>
2023-02-20 21:05:57 +0800

    faster

--- dblp_crawler/summarizer/neo4j/n4j.py
+++ dblp_crawler/summarizer/neo4j/n4j.py
@@ -10,9 +10,12 @@ from dblp_crawler import Graph, DBLPPerson, Publication
 logger = logging.getLogger("graph")
 
 
-def add_person(tx, person: DBLPPerson):
+def add_person(tx, person: DBLPPerson, added_pubs: set):
     tx.run("MERGE (a:Person {pid: $pid, name: $name})", pid=person.pid(), name=person.name())
     for publication in person.publications():
+        if publication.key() in added_pubs:
+            continue
+        added_pubs.add(publication.key())
         tx.run("MATCH (a:Person) WHERE a.pid = $pid "
                "MERGE (p:Publication {key:$key, title:$title, journal_key:$journal_key, journal:$journal, year:$year}) "
                "MERGE (a)-[:WRITE]->(p)",
@@ -42,10 +45,11 @@ class Neo4jGraph(Graph, metaclass=abc.ABCMeta):
     def __init__(self, session: Session, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.session = session
+        self.added_pubs = set()
 
     def summarize_person(self, a: str, person: Optional[DBLPPerson]):  # 构建summary
         if person is not None:
-            self.session.execute_write(add_person, person)  # 把作者信息加进图里
+            self.session.execute_write(add_person, person, self.added_pubs)  # 把作者信息加进图里
 
     def summarize_publication(self, a: str, b: str, publication: Publication):  # 构建summary
         self.session.execute_write(add_edge, a, b, publication)  # 把边加进图里
