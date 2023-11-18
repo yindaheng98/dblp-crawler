1d33916f3408cbdabdaf9d8311c51c9764f7c0af
yindaheng98 <yindaheng98@gmail.com>
2023-11-17 22:39:21 -0800

    simplify

--- dblp_crawler/summarizer/networkx/nx.py
+++ dblp_crawler/summarizer/networkx/nx.py
@@ -1,5 +1,4 @@
 import abc
-import asyncio
 import json
 import logging
 
@@ -10,39 +9,36 @@ from dblp_crawler import Graph
 logger = logging.getLogger("graph")
 
 
+def summary_person(person, publications):  # 构建summary
+    return dict(
+        person=person, publications=list(publications.values())
+    )
+
+
+def summary_cooperation(a, b, publications):  # 构建summary
+    a_pub = set(p.key() for p in a.publications())
+    b_pub = set(p.key() for p in b.publications())
+    return dict(publications=list(publications.values()), cooperation=list(a_pub.intersection(b_pub)))
+
+
 class NetworkxGraph(Graph, metaclass=abc.ABCMeta):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.graph: nx.MultiGraph = nx.MultiGraph()
 
     def summarize_person(self, a, person):  # 构建summary
-        self.graph.add_node(a, person=self.persons[a])  # 把作者信息加进图里
+        self.graph.add_node(a, person=person)  # 把作者信息加进图里
 
     def summarize_publication(self, a, b, publication):  # 构建summary
         self.graph.add_edge(a, b, key=publication.key(), publication=publication)  # 把边加进图里
 
-    def multi_graph_summary(self):
-        """输出一个 networkx.MultiGraph，节点对应作者，每条边对应一篇论文，作者间可有多条边"""
-        self.graph = nx.MultiGraph()
-        self.summarize()
-        return self.graph
-
-    def summary_person(self, person, publications):  # 构建summary
-        return dict(
-            person=person, publications=list(publications.values())
-        )
-
-    def summary_cooperation(self, a, b, publications):  # 构建summary
-        a_pub = set(p.key() for p in a.publications())
-        b_pub = set(p.key() for p in b.publications())
-        return dict(publications=list(publications.values()), cooperation=list(a_pub.intersection(b_pub)))
-
     def graph_summary(self):
         """输出一个 networkx.Graph，节点对应作者，每条边对应多篇论文，作者间仅有一条边"""
-        g = self.multi_graph_summary()
+        self.graph = nx.MultiGraph()
+        self.summarize()
         gg = nx.Graph()
         authors = {}
-        for (a, b, d) in g.edges(data=True):  # 遍历所有文章
+        for (a, b, d) in self.graph.edges(data=True):  # 遍历所有文章
             publication = d['publication']
             # 把文章加进边信息里
             data = gg.get_edge_data(a, b)
@@ -58,10 +54,10 @@ class NetworkxGraph(Graph, metaclass=abc.ABCMeta):
                 authors[b] = {}
             authors[b][publication.key()] = publication
         for pid, publications in authors.items():
-            gg.add_node(pid, **self.summary_person(g.nodes[pid]['person'], publications))
+            gg.add_node(pid, **summary_person(self.graph.nodes[pid]['person'], publications))
         for (a, b, d) in list(gg.edges(data=True)):  # 遍历所有文章
-            gg.add_edge(a, b, **self.summary_cooperation(
-                g.nodes[a]['person'], g.nodes[b]['person'], d["publications"]
+            gg.add_edge(a, b, **summary_cooperation(
+                self.graph.nodes[a]['person'], self.graph.nodes[b]['person'], d["publications"]
             ))
         return gg
 
