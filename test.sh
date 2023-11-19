c9ccd930abbd7f2c917db1bacdd2f53c2f9cfded
yindaheng98 <yindaheng98@gmail.com>
2023-11-19 13:58:32 -0800

    junk

--- test.sh
+++
@@ -1,2 +0,0 @@
-python -m dblp_crawler -k "'super','resolution'" -k video -k "'edge'" -p l/JiangchuanLiu -y 2016 networkx --dest summary.json
-python -m dblp_crawler -k "'super','resolution'" -k video -k "'edge'" -p l/JiangchuanLiu -y 2016 neo4j --uri neo4j://10.128.202.18:7687
